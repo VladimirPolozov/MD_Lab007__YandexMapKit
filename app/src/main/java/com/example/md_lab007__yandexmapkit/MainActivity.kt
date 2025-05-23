0485a2cd1ac1a1a4d99cab3e8bde96b74328a478
Vladimir Polozov <z2005q.polozov@yandex.ru>
2025-05-24 01:51:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/md_lab007__yandexmapkit/MainActivity.kt
@@ -0,0 +1,47 @@
+    package com.example.md_lab007__yandexmapkit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.md_lab007__yandexmapkit.ui.theme.MD_Lab007__YandexMapKitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MD_Lab007__YandexMapKitTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MD_Lab007__YandexMapKitTheme {
+        Greeting("Android")
+    }
+}
